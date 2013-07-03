d039203d9ecaa9bef9759b72829972c6bedbdeaf
Chris Roos <chris@seagul.co.uk>
2013-07-03 17:25:12 +0100

    Empty Rails 4.0.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WemCucumberRails4::Application.config.secret_key_base = 'cda587207af27176d76f4dac2925ced9a5049b140d772a67d33e10dbfe0e80c4b47b7da8693fb0bf54aedf706e225d6b12e5ffef1170ba0e735adfd2ec1a22a1'
